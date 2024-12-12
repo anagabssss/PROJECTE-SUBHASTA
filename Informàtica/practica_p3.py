d4df8948be298cd9046815113a2546798ba41c97
ainaboncat <128025962+ainaboncat@users.noreply.github.com>
2024-12-12 19:22:07 +0100

    p3

---
+++ Informàtica/practica_p3.py
@@ -0,0 +1,12 @@
+str = "" # ''
+int = 0
+float = 0.0
+list = []
+dict = {}
+
+dict = {1:9.5,22222222:8.4,33333333:3.5,44444444:9.3}
+print(dict) # escribe el diccionari
+print(dict.items()) # escribe el diccionario como una lista y las parejas en tuplas
+
+1 in dict # --> True
+5 in dict # --> False
